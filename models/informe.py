c48da1026eb0fd10aadd539069033a08105e16c5
AdrianSVHappy <adriansuarezvaldayo573@gmail.com>
2025-02-26 20:08:17 +0100

    Generador de nombre / identificador automático para los informes

--- models/informe.py
+++ models/informe.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 """
 Informes -> investigador, experimento, materiales, texto 
@@ -9,18 +10,21 @@ class lab_informe(models.Model):
     _description = 'Informe'
     
     #Campos
-    name = fields.Char(string="ID", select=True)
 
-    
+
     #Relaciones
     redactor = fields.Many2one('lab.investigador')
     experimento = fields.Many2one('lab.experimento')
     
     #Calculado
+    name = fields.Char(string="ID", select=True, compute="_setId")
     texto = fields.Text(inverse="_autoText")
-#    texto = fields.Text()
+
     
     @api.constrains(redactor, experimento)
     def _autoText(self):
         if self.experimento and self.redactor:
             self.texto = f"Informe del experimento {self.experimento.name}.\nRedactado por {self.redactor.name} {self.redactor.apellidos}\n"
+            
+    def _setId(self):
+        self.name = "INF-" + str(datetime.now())
